35f363e3d2f5d13b16d43e8b8ee1489dfc80c482
shuanghai.zhang <shuanghai.zhang@thoughtworks.com>
2020-03-01 21:32:11 +0800

    refactor: feature envy - move update quality to Item

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -19,27 +19,6 @@ class GildedRose {
     }
 
     private void updateQualityByName() {
-        for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie":
-                    item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
-                    item.quality = (item.quality < 50 && item.shelfLife < 0) ? item.quality + 1 : item.quality;
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                        item.quality = item.shelfLife < 12 ? item.quality + 1 : item.quality;
-                        item.quality = item.shelfLife < 7 ? item.quality + 1 : item.quality;
-                        item.quality = Math.min(50, item.quality);
-                    }
-                    item.quality = item.shelfLife < 0 ? 0 : item.quality;
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    break;
-                default:
-                    item.quality = item.quality > 0 ? item.quality - 1 : item.quality;
-                    item.quality = (item.quality > 0 && item.shelfLife < 0) ? item.quality - 1 : item.quality;
-            }
-        }
+        Arrays.stream(items).forEach(Item::updateQualityByName);
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -20,6 +20,29 @@ public class Item {
         }
     }
 
+    public void updateQualityByName() {
+        switch (name) {
+            case "Aged Brie":
+                quality = quality < 50 ? quality + 1 : quality;
+                quality = (quality < 50 && shelfLife < 0) ? quality + 1 : quality;
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (quality < 50) {
+                    quality = quality + 1;
+                    quality = shelfLife < 12 ? quality + 1 : quality;
+                    quality = shelfLife < 7 ? quality + 1 : quality;
+                    quality = Math.min(50, quality);
+                }
+                quality = shelfLife < 0 ? 0 : quality;
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                quality = quality > 0 ? quality - 1 : quality;
+                quality = (quality > 0 && shelfLife < 0) ? quality - 1 : quality;
+        }
+    }
+
     @Override
     public String toString() {
         return this.name + ", " + this.shelfLife + ", " + this.quality;
