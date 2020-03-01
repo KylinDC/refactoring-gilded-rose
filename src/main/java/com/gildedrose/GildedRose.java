859c0fcd3a60b257fae1c63b56abf9ded133daf8
shuanghai.zhang <shuanghai.zhang@thoughtworks.com>
2020-03-01 19:35:11 +0800

    refactor: using for-each statement instead of for loop

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -13,28 +13,28 @@ class GildedRose {
 
         updateSellIn();
 
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        for (Item value : items) {
+            if (!value.name.equals("Aged Brie")
+                    && !value.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (value.quality > 0) {
+                    if (!value.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        value.quality = value.quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (value.quality < 50) {
+                    value.quality = value.quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sell_in < 12) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                    if (value.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (value.sell_in < 12) {
+                            if (value.quality < 50) {
+                                value.quality = value.quality + 1;
                             }
                         }
 
-                        if (items[i].sell_in < 7) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (value.sell_in < 7) {
+                            if (value.quality < 50) {
+                                value.quality = value.quality + 1;
                             }
                         }
                     }
@@ -42,21 +42,21 @@ class GildedRose {
             }
         }
 
-        for (int i = 0; i < items.length; i++) {
-            if (items[i].sell_in < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+        for (Item item : items) {
+            if (item.sell_in < 0) {
+                if (!item.name.equals("Aged Brie")) {
+                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
                 }
             }
