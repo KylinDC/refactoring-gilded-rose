a93693892485a99c27bc3a098b2484d61a0a0d32
shuanghai.zhang <shuanghai.zhang@thoughtworks.com>
2020-03-01 21:37:59 +0800

    refactor: comments - remove useless comments

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -23,16 +23,15 @@ public class GildedRoseTest {
     @Test
     public void more_gilded_rose_item_test() {
         Item[] items = new Item[]{
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("+5 Dexterity Vest", 10, 20),
+                new Item("Aged Brie", 2, 0),
+                new Item("Elixir of the Mongoose", 5, 7),
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
-                // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6)};
 
         GildedRose gildedRose = new GildedRose(items);
